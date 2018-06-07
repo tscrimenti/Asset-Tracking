f5fb0480befa7ec7e357f952f66b4400c7094df0
Tony <anthony.scrimenti@gmail.com>
2018-06-07 11:24:44 -0400

    Remove Alerts

--- platforms/browser/www/js/jquery.js
+++ platforms/browser/www/js/jquery.js
@@ -1,6 +1,11 @@
 $(document).ready(function(){
 //alert("Document Read"); 
 
+document.addEventListener("deviceready", onDeviceReady, false);
+function onDeviceReady() {
+    console.log(navigator.camera);
+}
+
 $("#scan_button").on("click touch", function() {
        cordova.plugins.barcodeScanner.scan(
             function (result) {
@@ -77,6 +82,51 @@ $(document).on("click touch","#save_and_done", function () {
 			     }); 
 
 }); 
+
+//******************************************************************************
+//                   TAKE PICTURE 
+//******************************************************************************
+
+$(document).on("click touch", "#take_picture", function () {
+   console.log("lets take a picture"); 
+	 alert("lets take a picture"); 
+	 
+	 function setOptions(srcType) {
+    var options = {
+        // Some common settings are 20, 50, and 100
+        quality: 50,
+        destinationType: Camera.DestinationType.FILE_URI,
+        // In this app, dynamically set the picture source, Camera or photo gallery
+        sourceType: srcType,
+        encodingType: Camera.EncodingType.JPEG,
+        mediaType: Camera.MediaType.PICTURE,
+        allowEdit: true,
+        correctOrientation: true  //Corrects Android orientation quirks
+    }
+    return options;
+}
+/**/
+function openCamera(selection) {
+
+    var srcType = Camera.PictureSourceType.CAMERA;
+    var options = setOptions(srcType);
+    var func = createNewFileEntry;
+
+    navigator.camera.getPicture(function cameraSuccess(imageUri) {
+
+        displayImage(imageUri);
+        // You may choose to copy the picture, save it somewhere, or upload.
+        func(imageUri);
+
+    }, function cameraError(error) {
+        console.debug("Unable to obtain picture: " + error, "app");
+
+    }, options);
+} 
+	 
+
+}); 
+
 
 
 }); 
